0839f10bdd05f9728f2c38abfa4eb676f9565e7a
Tinus Wagner <tinus@3months.com>
2016-08-15 21:26:20 +1200

    pew got something up

--- components/todos.jsx
+++ components/todos.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import NewTodo from './NewTodo'
-import { addTodo, deleteTodo } from '../actions'
-
-const Todos = ({todos, dispatch}) => (
+const Todos = ({todos}) => (
   <div>
     <h1>Todos</h1>
-    <NewTodo onChange={e => {
-      if(e.keyCode == 13){
-        dispatch(addTodo(e.target.value))
-        e.target.value = ''
-      }
-    }}/>
-    {todos.map((todo, index) => <p key={index}>{todo} <button onClick={e => {
-      dispatch(deleteTodo(index))
-    }}>X</button></p>)}
+    {todos.map(todo => <p key={todo}>{todo}</p>)}
   </div>
 )
 
